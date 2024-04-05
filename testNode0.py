45171c9825e0b660ecbd8a36a9e4ceba2ff41943
Gionatan Cernusco <gionatan99@gmail.com>
2024-04-05 11:47:15 +0200

    chore: splits nodes, adds welcome

--- testNode0.py
+++ testNode0.py
@@ -2,6 +2,10 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+@app.route('/', methods=['POST'])
+def welcome():
+    return jsonify({"message": "Welcome", "node": "0"});
+
 @app.route('/test', methods=['POST'])
 def generate_image():
     # Get the text prompt from the request
@@ -11,7 +15,7 @@ def generate_image():
     text_prompt = data['text']
     
     # Return the image as a response
-    return jsonify({"input": text_prompt, "pod": "1"}), 200
+    return jsonify({"input": text_prompt, "node": "0"}), 200
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ testNode1.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def welcome():
+    return jsonify({"message": "Welcome", "node": "1"});
+
+@app.route('/test', methods=['POST'])
+def generate_image():
+    # Get the text prompt from the request
+    data = request.get_json()
+    if not data or 'text' not in data:
+        return jsonify({"error": "Missing 'text' field in the request body"}), 400
+    text_prompt = data['text']
+    
+    # Return the image as a response
+    return jsonify({"input": text_prompt, "node": "1"}), 200
+
+if __name__ == '__main__':
+    app.run(debug=True)
