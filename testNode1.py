12a3eb56c038b1970d459ae54b4d0718d063834e
Gionatan Cernusco <gionatan99@gmail.com>
2024-04-15 12:04:45 +0200

    fix: sets GET method on welcome route

--- testNode1.py
+++ testNode1.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def welcome():
     return jsonify({"message": "Welcome", "node": "1"});
 
